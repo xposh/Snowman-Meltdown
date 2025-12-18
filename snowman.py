5fbf8e7c2336d9ecfedeed060efada837bddd197
Posh <posh@Mac.fritz.box>
2025-12-18 14:10:52 +0100

    Game Update

--- snowman.py
+++ snowman.py
@@ -3,9 +3,11 @@ import random
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
+
 def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
+
 
 def play_game():
     secret_word = get_random_word()
@@ -16,3 +18,7 @@ def play_game():
     # For now, simply prompt the user once:
     guess = input("Guess a letter: ").lower()
     print("You guessed:", guess)
+
+
+if __name__ == "__main__":
+    play_game()
