bc8170e2c0baa54eb381b43286246a5b3347f888
Posh <posh@MacBook-Air-von-Posh.fritz.box>
2025-12-18 15:43:17 +0100

    Integrate snowman ASCII art

--- snowman.py
+++ snowman.py
@@ -1,5 +1,35 @@
 import random
 
+# Snowman ASCII Art stages
+STAGES = [
+    # Stage 0: Full snowman
+    """
+     ___  
+    /___\\ 
+    (o o) 
+    ( : ) 
+    ( : ) 
+    """,
+    # Stage 1: Bottom part starts melting
+    """
+     ___  
+    /___\\ 
+    (o o) 
+    ( : ) 
+    """,
+    # Stage 2: Only the head remains
+    """
+     ___  
+    /___\\ 
+    (o o) 
+    """,
+    # Stage 3: Snowman completely melted
+    """
+     ___  
+    /___\\ 
+    """
+]
+
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
@@ -9,13 +39,30 @@ def get_random_word():
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
+def display_game_state(mistakes, secret_word, guessed_letters):
+    # Display the snowman stage for the current number of mistakes.
+    print(STAGES[mistakes])
+    # Build a display version of the secret word.
+    display_word = ""
+    for letter in secret_word:
+        if letter in guessed_letters:
+            display_word += letter + " "
+        else:
+            display_word += "_ "
+    print("Word: ", display_word)
+    print("\n")
+
+
 def play_game():
     secret_word = get_random_word()
-    print("Welcome to Snowman Meltdown!")
-    print("Secret word selected: " + secret_word)  # for testing, later remove this line
+    guessed_letters = []
+    mistakes = 0
 
-    # TODO: Build your game loop here.
-    # For now, simply prompt the user once:
+    print("Welcome to Snowman Meltdown!")
+    # For now, display the initial game state.
+    display_game_state(mistakes, secret_word, guessed_letters)
+
+    # Prompt user for one guess (logic to be enhanced later)
     guess = input("Guess a letter: ").lower()
     print("You guessed:", guess)
 
